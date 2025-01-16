7a5ab449ba513d816453d6447a6c397a5b777f6f
Denis Anatoliev <denisanatcode@gmail.com>
2025-01-16 20:53:50 +0000

    Commit Serveis RetailDenis

--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -6,8 +6,17 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Service\ServeiDadesSeccio;
+
 class IniciController extends AbstractController
 {
+	private $seccions;
+
+	public function __construct($dadesSeccions)
+	{
+		$this->seccions = $dadesSeccions->get();
+	}
+
 	#[Route('/' ,name:'home')]
 	public function home()
 	{
@@ -17,7 +26,7 @@ class IniciController extends AbstractController
 	#[Route('/inici' ,name:'inici')]
 	public function inici()
 	{
-		return $this->render('inici.html.twig');
+		return $this->render('inici.html.twig', array("seccions" => $this->seccions));
 	}
 }
 ?>
--- src/Controller/SeccioController.php
+++ src/Controller/SeccioController.php
@@ -6,31 +6,21 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use App\Service\ServeiDadesSeccio;
+
 class SeccioController extends AbstractController
 {
-	private $seccions = array(
-		array("codi" => "1", "nom" => "Electrodomèstics", 
-			"descripcio" => "Aparells electrònics per a la llar", "any" => "2025", 
-			"articles" => array("Llavadora", "Forn", "Rentaplats", "Ventilador"),
-			"img" => "assets/img/washing-machine-cartoon-vector-icon-illustration-object-technology-icon-concept-isolated-premium_138676-4690.jpg"),
-		array("codi" => "2", "nom" => "Components d'ordinador", 
-			"descripcio" => "Distintes parts que formen un ordinador", "any" => "2025", 
-			"articles" => array("Disc dur", "Memòria RAM", "Processador", "Font d'alimentació"),
-			"img" => "assets/img/elearning-workspace-concept-illustration_114360-25845.jpg"),
-		array("codi" => "3", "nom" => "Sucs", 
-			"descripcio" => "Sucs de distintes fruites", "any" => "2025", 
-			"articles" => array("Poma", "Taronja", "Pinya", "Kiwi"),
-			"img" => "assets/img/fresh-orange-juice-splashing-from-glass-transparent-background_84443-26351.jpg"),
-		array("codi" => "4", "nom" => "Contes infantils", 
-			"descripcio" => "Llibres per als més menuts", "any" => "2025", 
-			"articles" => array("La Ventafocs", "Les set cabretes", "Blancaneu", "La bella dorment"),
-			"img" => "assets/img/book-with-lighbulb-cartoon-vector-icon-illustration-object-education-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-4009.jpg"),
-	);
+	private $seccions;
+
+	public function __construct($dadesSeccions)
+	{
+		$this->seccions = $dadesSeccions->get();
+	}
 
 	#[Route('/seccio/{codi}', name: 'dades_seccions')]
 	public function seccio($codi)
 	{
-		$resultat = array_filter($this -> seccions, function($seccio) use ($codi) {
+		$resultat = array_filter($this->seccions, function($seccio) use ($codi) {
 			return $seccio["codi"] == $codi;
 		});
 
