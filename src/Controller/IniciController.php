01970b6549f64afb09bb7c903d798f4c4c7215a0
Denis Anatoliev <denisanatcode@gmail.com>
2025-01-16 13:40:05 +0000

    Commit Twig RetailDenis

--- src/Controller/IniciController.php
+++ src/Controller/IniciController.php
@@ -4,26 +4,20 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class IniciController
+class IniciController extends AbstractController
 {
-	private $contactes = array(
-	array("codi" => 1, "nom" => "Salvador Sala",
-	"telefon" => "638961244", "email" => "salvasala@simarro.org"),
-	array("codi" => 2, "nom" => "Anna Llopis",
-	"telefon" => "669332004", "email" => "annallopis@simarro.org"),
-	array("codi" => 3, "nom" => "Marc Sanchis",
-	"telefon" => "962286040", "email" => "marcsanchis@simarro.org"),
-	array("codi" => 4, "nom" => "Laura Palop",
-	"telefon" => "663568890", "email" => "laurapalop@simarro.org"),
-	array("codi" => 5, "nom" => "Sara Sidle",
-	"telefon" => "638765434", "email" => "sarasidle@simarro.org"),
-	);
+	#[Route('/' ,name:'home')]
+	public function home()
+	{
+		return $this -> render('base.html.twig');
+	}
 
-	#[Route('/' ,name:'inici')]
+	#[Route('/inici' ,name:'inici')]
 	public function inici()
 	{
-		return new Response("<h1>Projecte Gestió Retail de 2n de DAW<h1>");
+		return $this->render('inici.html.twig');
 	}
 }
 ?>
--- src/Controller/SeccioController.php
+++ src/Controller/SeccioController.php
@@ -4,25 +4,30 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class SeccioController
+class SeccioController extends AbstractController
 {
 	private $seccions = array(
 		array("codi" => "1", "nom" => "Electrodomèstics", 
 			"descripcio" => "Aparells electrònics per a la llar", "any" => "2025", 
-			"articles" => array("Llavadora", "Forn", "Rentaplats", "Ventilador")),
+			"articles" => array("Llavadora", "Forn", "Rentaplats", "Ventilador"),
+			"img" => "assets/img/washing-machine-cartoon-vector-icon-illustration-object-technology-icon-concept-isolated-premium_138676-4690.jpg"),
 		array("codi" => "2", "nom" => "Components d'ordinador", 
 			"descripcio" => "Distintes parts que formen un ordinador", "any" => "2025", 
-			"articles" => array("Disc dur", "Memòria RAM", "Processador", "Font d'alimentació")),
+			"articles" => array("Disc dur", "Memòria RAM", "Processador", "Font d'alimentació"),
+			"img" => "assets/img/elearning-workspace-concept-illustration_114360-25845.jpg"),
 		array("codi" => "3", "nom" => "Sucs", 
 			"descripcio" => "Sucs de distintes fruites", "any" => "2025", 
-			"articles" => array("Poma", "Taronja", "Pinya", "Kiwi")),
+			"articles" => array("Poma", "Taronja", "Pinya", "Kiwi"),
+			"img" => "assets/img/fresh-orange-juice-splashing-from-glass-transparent-background_84443-26351.jpg"),
 		array("codi" => "4", "nom" => "Contes infantils", 
 			"descripcio" => "Llibres per als més menuts", "any" => "2025", 
-			"articles" => array("La Ventafocs", "Les set cabretes", "Blancaneu", "La bella dorment")),
+			"articles" => array("La Ventafocs", "Les set cabretes", "Blancaneu", "La bella dorment"),
+			"img" => "assets/img/book-with-lighbulb-cartoon-vector-icon-illustration-object-education-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-4009.jpg"),
 	);
 
-	#[Route('/seccio/{codi}', name: 'dades_seccio')]
+	#[Route('/seccio/{codi}', name: 'dades_seccions')]
 	public function seccio($codi)
 	{
 		$resultat = array_filter($this -> seccions, function($seccio) use ($codi) {
@@ -30,17 +35,8 @@ class SeccioController
 		});
 
 		if (count($resultat) > 0) {
-			$resposta = "";
-			$resultat = array_shift($resultat);
-			$resposta .= "<ul style='list-style: none'><li><h2>" . $resultat["nom"] . "</h2></li>" .
-				"<li><h3>" . $resultat["descripcio"] . "</h3></li>" . 
-				"<li>" . $resultat["any"] . "</li><ol>";
-				foreach($resultat["articles"] as $article)
-					$resposta .= "<li>" . $article . "</li>";
-				$resposta .= "</ol></ul>";
-			
-			return new Response("<html><body>$resposta</body></html>");
+			return $this -> render('dades_seccio.html.twig', array('seccio' => array_shift($resultat)));
 		} else
-			return new Response("No s'ha trobat la secció: $codi");
+			return $this -> render('dades_seccio.html.twig', array('seccio' => null));
 	}
 }
